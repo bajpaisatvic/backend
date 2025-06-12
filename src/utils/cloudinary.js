930ea753670911094019249a6760ded9422ae241
Satvic Bajpai <satvicbajpai@gmail.com>
2025-06-12 16:19:53 +0530

    updated cloudinary code

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -19,6 +19,7 @@ const uploadOnCloudinary = async (localFilePath) => {
 
     // file has been uploaded successfully
     console.log(`file is uploaded on cloudinary: ${response.url}`);
+    fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation got successful
     return response;
   } catch (error) {
     console.log("Error uploading file to Cloudinary:", error);
